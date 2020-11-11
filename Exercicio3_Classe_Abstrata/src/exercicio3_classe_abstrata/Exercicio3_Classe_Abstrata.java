8643eecc5a57e2b8bd6332869acb22ebc9f77fbb
Marcelo Pedroni da Silva <marcelopedronidasilva@gmail.com>
2020-11-10 22:10:07 -0300

    Exercício 3
    Definição da Superclasse abstrata Empregado, com atributos nome, cps e sobrenome e um método de vencimentos(), herdados pelas subclasses Assalariado; Horista e Comissionado, com as devidas modificações nas sobreposições para fins de cálculos de vencimentos. Na main, há uma instância para cada subclasse, exibindo nome, sobrenome, cpf e vencimentos.

---
+++ Exercicio3_Classe_Abstrata/src/exercicio3_classe_abstrata/Exercicio3_Classe_Abstrata.java
@@ -0,0 +1,38 @@
+package exercicio3_classe_abstrata;
+
+public class Exercicio3_Classe_Abstrata {
+
+    public static void main(String[] args) {
+           
+        Empregado e1 = new Assalariado("Marcelo", "Pedroni", "00000000");
+        System.out.println("Nome: " + e1.getNome());
+        System.out.println("Sobrenome: " + e1.getSobrenome());
+        System.out.println("CPF: " + e1.getCpf());
+        System.out.println("Total de vencimentos: " + e1.vencimento());
+        System.out.println("=========");
+                
+        Comissionado e2 = new Comissionado("Creusa", "Silva", "0921731");
+        e2.setTaxaComissao(0.3);
+        e2.setTotalVenda(20000);
+        System.out.println("Nome: " + e2.getNome());
+        System.out.println("Sobrenome: " + e2.getSobrenome());
+        System.out.println("CPF: " + e2.getCpf());
+        System.out.println("Total de Vendas: " + e2.getTotalVenda());
+        System.out.println("Taxa de Comissão: " + e2.getTaxaComissao() +"%");
+        System.out.println("Total de vencimentos: " + e2.vencimento());
+        System.out.println("=========");
+        
+        Horista e3 = new Horista("Odirlei", "Botânico", "3287833-44");
+        e3.setHorasTrabalhadas(225);
+        e3.setPrecoHora(25);
+        System.out.println("Nome: " + e3.getNome());
+        System.out.println("Sobrenome: " + e3.getSobrenome());
+        System.out.println("CPF: " + e3.getCpf());
+        System.out.println("Total de Horas Trabalhadas: " + e3.getHorasTrabalhadas());
+        System.out.println("Preço da Hora R$: " + e3.getPrecoHora());
+        System.out.println("Total de vencimentos: " + e3.vencimento());
+        System.out.println("=========");
+        
+    }
+
+}
